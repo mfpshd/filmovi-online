70f6984c08d18cc74e262a25bfb3d8f81dd548a4
Filmovi online sa prevodom <95509035+mfpshd@users.noreply.github.com>
2021-12-04 00:51:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,264 @@ export default function Home() {
     </div>
   )
 }
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+	
+    <meta name="description" content="">
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<link rel="shortcut icon" type="image/x-icon" href="favicon02.ico" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.0/css/bulma.min.css">
+	<meta name=robots content="index,follow">
+	<meta name=googlebot content="index,follow">
+    
+    <script type="application/ld+json">
+{
+  "@context": "https://schema.org/", 
+  "@type": "Article", 
+  "author": {
+    "@type": "Person",
+    "name": "Fred Maxwell"
+  },
+  "headline": "The Last Duel",
+  "datePublished": "2020-09-08",
+  "image": "https:",
+  "publisher": {
+    "@type": "Organization",
+    "name": "SJA Team",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://via.placeholder.com/512.png?text=SJA Team",
+      "width": 512,
+      "height": 512
+    }
+  }
+}
+</script>
+    
+
+    <title>[Cinestar™] The Last Duel °Gledati~ 2021 Cijeli Film S Hrvatskim FULL HD (1080P)</title>
+    <style>
+	.showcase {
+    background-color: #f7f7f7;
+    margin-bottom: 4em;
+    margin-top: 2em;
+    padding: 2em;
+    border-radius: .5em;
+	}
+	hr {
+    background-color: #dbdbdb;
+    border: none;
+    display: block;
+    height: 1px;
+    margin: 1.5rem 0;
+	}
+
+	</style>
+	
+<!-- 01. Resep --> <script async="async" data-cfasync="false" src="//pl15776955.cpmnetworkcontent.com/9ecae32596774d9f341fd3cffec9ca45/invoke.js"></script> 
+<!-- 02. Film  <script async="async" data-cfasync="false" src="//pl15780932.cpmprofitablecontent.com/7d5405c1a3899927973b6406c34ea6b6/invoke.js"></script>
+-->
+</head>
+<body class="has-background-grey-lighter">
+	<nav class="navbar has-background-bis" role="navigation" aria-label="main navigation">
+	<div class="container">
+	  <div class="navbar-brand">
+		<a class="navbar-item" href="index.html">
+			<span class="icon is-medium"><i class="fas fa-circle fa-2x" aria-hidden="true"></i></span><p>&nbsp;&nbsp;&nbsp;Film CEO 4U</p>
+		</a>
+
+		<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+		  <span aria-hidden="true"></span>
+		  <span aria-hidden="true"></span>
+		  <span aria-hidden="true"></span>
+		</a>
+	  </div>
+
+	  <div id="navbarBasicExample" class="navbar-menu">
+		<div class="navbar-end">
+		
+		  <a class="navbar-item" href="index.html">
+			Index
+		  </a>
+		
+		  <a class="navbar-item" href="contact.html">
+			Contact
+		  </a>
+		
+		  <a class="navbar-item" href="copyright.html">
+			Copyright
+		  </a>
+		
+		  <a class="navbar-item" href="dmca.html">
+			DMCA
+		  </a>
+		
+		  <a class="navbar-item" href="privacy-policy.html">
+			Privacy-Policy
+		  </a>
+		
+		</div>
+	  </div>
+	</div>
+	</nav>
+
+
+<div class="container">
+            <div class="column is-12">
+                <!-- START ARTICLE -->
+                <div class="card article">
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content has-text-centered">
+								<h1 class="title is-capitalized">'Gledati The Last Duel 『Online』 puni FILM [2021]</h1>
+								<img src="//image.tmdb.org/t/p/w185" alt="The Last Duel">
+                            </div>
+                        </div>
+                        <div class="content article-body">
+<p>
+<center>
+Prije 17 sati - Gledaj** The Last Duel 2020 CEO Film Online HD<br>
+[21-CINE] The Last Duel Filmovi Online sa prevodom
+<br><br>
+**Gledanje filmova putem interneta ili streaming zaista je izbor mnogih ljudi danas. Oružani s web mjesta koja na internetu gleda filmove sada**
+<br>
+The Last Duel Filmuvi Online
+</center>
+</p>
+<p>
+	
+</p>
+<p>
+	
+</p>
+
+
+<div class="has-text-centered">
+<a href="http://moviefor.pro/movie/617653"> <img class="has-image-centered" src="https://1.bp.blogspot.com/-a5_bLCkafBI/Xy0-pdXedOI/AAAAAAAAAAU/M0NHwatww-4LmHN4ee93-kMAZ-6Jc86UQCLcBGAsYHQ/s0/iCAWqmO.jpg" alt="The Last Duel"></a>
+</div>
+
+
+<p>
+	
+</p>
+
+<h3 class="has-text-centered">Trailer The Last Duel</h3>
+						<figure class="image is-16by9">
+							<iframe class="has-ratio" src="//www.youtube.com/embed/?rel=0&modestbranding=1&autoplay=0&autohide=1&showinfo=1&controls=0;"></iframe>
+						</figure>
+
+<script async="async" data-cfasync="false" src="//pl15780933.cpmprofitablecontent.com/78559c023923616fd521e6555b542d3f/invoke.js"></script>
+<div id="container-78559c023923616fd521e6555b542d3f"></div>
+
+<p>
+	
+</center>
+</p>
+
+<p>
+	
+</p>
+<p>
+<center>
+**[Gledaj] The Last Duel (2020) Filmovi Online sa Your browser indicates if you've visited this linkhttps medium com/hrvatski/gledaj-The Last Duel-2020-filmovi-online-sa-prevodom-film-besplatno-baf42ec3676fNovi Filmovi Online sa Prevodom u HD Rezoluciji The Last Duel Online filmovi sa prevodom za gledanje potpuno besplatno! Novi domaci i strani — veliki broj naslova raznih žanrova The Last Duel (2003) online sa prevodom - Popcorn Srbija
+<br>
+Your browser indicates if you've visited this link
+<br>
+https popcornsrbija com/The Last Duel-2003The Last Duel (2003) online sa prevodom Sasvim neocekivani trag ih dovodi do velike urote u koju je ukljucena mafija na celu s narko bossom Johnnyjem Tapiom (Jordi Molla), ciji je krajnji cilj preuzeti celokupnu trgovinu droge u gradu, makar to znacilo i veliki medjunarodni obracun onih kojima se ta njihova namera ne svidja Mike se pritom i Zločesti dečki (Bad Boys, 1995) - Film Your browser indicates if you've visited this linkhttps mojtv hr/film/1993/The Last Duel - Dva policajca iz odjela za narkotike istražuju nestanak heroina iz policijskog skladišta - Pogledajte video i opis za film The Last Duel je solidan akcijski film, sa elementima komedije Film je sniman prema klasičnom receptu akcijskih filmova i ispao je sasvim ok #6 0 0 Videos46:19Porodica mog oca - Epizoda 2A644 viewsDailymotion1:43:24Serija - Zabrana 2 epizoda sa prevodom299 viewsYouTube50:05Zavet - 3 Epizoda2 3K viewsDailymotion48:56Crusades 2 - Jerusalem (sa prevodom)10 viewsDailymotion1:46:12Pljujem ti na grob 2 ! (Horor film sa prevodom)5 9K viewsYouTube47:18Esperança 1 epizoda1 3K viewsDailymotion42:28The Winds of War IV - Defiance (sa prevodom) part 2/231 viewsDailymotion1:43:29Yemin / Zakletva 2 sezona (71 epizoda)34K viewsok ru1:48:17Pljujem ti na grob 1 (Horor film sa prevodom)6 9K viewsYouTube59:22Serija Ljubav na Silu Epizoda 2 Sa Prevodom609 viewsDailymotionMore VideosAre these links helpful?YesNo[Hrvatskim] The Last Duel Cijeli Film Online Sa Your browser indicates if you've visited this linkhttps hrvisioncine wordpress com/2020/01/28/hrvatskim-zlocesti-decki-zauvijek-cijeli-film-online-sa-prevodom/The Last Duel Online Sa Prevodom Gledati The Last Duel Film Online Sa Prevodom, The Last Duel Cijeli Film, Gledati The Last Duel, The Last Duel Preuzimanja, The Last Duel Podnaslov Hrvatski, The Last Duel Online Gledanje Gledajte Sada Ovdje Žanr: akcija, komedija / Redatelj: Adil El Arbi, Bilall Fallah Glumci (Bad Boys 2, 2003) - FilmYour browser indicates if you've visited this linkhttps mojtv hr/film/2006/The Last Duel aspx - Policajci W Smith i M Lawrence imaju novi zadatak - otkriti dobavljače ecstasyja u Miamiju - Pogledajte video i opis za film (The Last Duel)[720p-HD] The Last Duel (2020) Online film sa Your browser indicates if you've visited this linkhttps filmovigledajtv wordpress com/720p-hd-zlocesti-decki-zauvijek-2020-online-film-sa-prevodom/[Filmovi-HD] The Last Duel Online sa prevodom - Cinestar The Last Duel (2020) Filmuvi Online Originalni naslov: Bad Boys for Life U kinu od : 16 01 2020 Žanr : akcija, komedija SADRŽAJ FILMA: Zločesti dečki Mike Lowrey (Will Smith) i Marcus Burnett (Martin Lawrence) ponovno su zajedno u još jednoj ludoj vožnji u dugo očekivanom nastavku… 2003 Film Sa Prevodom Online HD | *Filmovi*Your browser indicates if you've visited this linkhttps growthcommissionblog org/film/8961/The Last Duel/ Film Sa Prevodom Besplatno Gledanje, Film Sa Prevodom Online HD! 2003 U ovoj novoj avanturi, miamijski narko detektivi Mike Lowrey (Will Smith) i Marcus Burnett (Martin Lawrence) dodijeljeni su kao dio visokotehnološkog tima koji pokušava otkriti zaplet dizajna ekstaze u Miamiju Ali oni nesvjesno otkrivaju smrtonosnu zavjeru u koju je upleten Cinestar The Last Duel gledaj online Prevodom HD Your browser indicates if you've visited this linkhttps sito libero it/cinestar4konline/2019/12/29/cinestar-zlocesti-decki-zauvijek-gledaj-online/The Last Duel gledaj online,puni film online, Online Sa Prevodom,Sila Se Budi ,filmove online gledati,Sa Prevodom HD,The Last Duel zlo likovi,The Last Duel zlo film,The Last Duel zlo sila se budi online sa prevodom, SADRŽAJ FILMA: Zločesti dečki Mike Lowrey (Will Smith) i Marcus Burnett (Martin 『Gledaj』 The Last Duel 『2020』 Online Sa Prevodom Your browser indicates if you've visited this linkhttps gledati-zlocesti-decki-zauvijek-2020-online-sa-prevodom peatix comThe Last Duel, The Last Duel gledaj, The Last Duel preuzimanje, The Last Duel online sa prevodom, The Last Duel cinestar, The Last Duel online, The Last Duel blitz cinestar, The Last Duel cineplexx, The Last Duel film 2019, The Last Duel (1995) - Online film sa prevodom - Filmovi euYour browser indicates if you've visited this linkhttps filmovi eu/online-sa-prevodom/The Last Duel - Miami je poznat kao glavni centar za distribuciju droge Marcus Burnett (Martin Lawrence) i Mike Lowrey (Will Smith) su dva policajca iz Odjela za narkotike kojima je povjeren nemoguć zadatak Naime, netko je počistio 100 milijuna dolara vrijedan heroin iz policijskog skladišta i policija ima 72 sata da riješi slučaj, u suprotnom …More results
+<br><br>
+The Last Duel TAGS :
+<br>
+The Last Duel Online Sa Prevodom, HD 1080p Gledati The Last Duel Online Sa Prevodom, puni film s prijevodima Označiti:: Gledajte sad!! The Last Duel
+<br>
+The Last Duel puni film online<br>
+The Last Duel puni film besplatno online<br>
+The Last Duel puni film download<br>
+The Last Duel Online Sa Prevodom HD 1080p Gledati<br>
+The Last Duel Online Sa Prevodom 1080p gledati filmove<br>
+The Last Duel puni film online<br>
+The Last Duel Online Sa Prevodom
+kwebeq HD FILM Gledati
+</center>
+</p>
+
+                        </div>
+                    </div>
+                </div>
+              </div>
+
+        <!-- END ARTICLE -->
+</div>
+
+<!-- 01. Resep	-->
+	<div class="container has-text-centered ">
+		<div id="container-9ecae32596774d9f341fd3cffec9ca45"></div>
+	</div>
+<!-- 02. Film	
+	<div class="container has-text-centered ">
+		<div id="container-7d5405c1a3899927973b6406c34ea6b6"></div>
+	</div>
+-->
+	
+    <footer style="padding-top: 50px;">
+    </footer>
+
+	
+
+
+<script type="text/javascript" defer>
+/* First CSS File */
+var GodOfSpeed = document.createElement('link');
+GodOfSpeed.rel = 'stylesheet';
+GodOfSpeed.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+GodOfSpeed.type = 'text/css';
+var godefer = document.getElementsByTagName('link')[0];
+godefer.parentNode.insertBefore(GodOfSpeed, godefer);
+
+</script>
+
+<script type="text/javascript">
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "stats.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script>
+</body>
+
+</html>
